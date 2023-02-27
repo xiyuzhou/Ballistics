7767feb096366aa842cfd66e025a55c758f8dd0c
xiyuzhou <xyz2333@outlook.com>
2023-02-26 17:16:25 -0700

    File via Upload

---
+++ Scripts/Frog.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frog : MonoBehaviour
+{
+    public float myTimeScale = 1.0f;
+    public GameObject target;
+    public float launchForce = 10f;
+    Rigidbody rb;
+    Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        Time.timeScale = myTimeScale; // allow for slowing time to see what's happening
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("go");
+            FiringSolution fs = new FiringSolution();
+            Nullable<Vector3> aimVector = fs.Calculate(transform.position, target.transform.position, launchForce, Physics.gravity);
+            Debug.Log(aimVector);
+            if (aimVector.HasValue)
+            {
+                Debug.Log("go2");
+                rb.AddForce(aimVector.Value.normalized * launchForce, ForceMode.VelocityChange);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // reset
+            rb.isKinematic = true;
+            transform.position = startPos;
+            rb.isKinematic = false;
+        }
+    }
+}
